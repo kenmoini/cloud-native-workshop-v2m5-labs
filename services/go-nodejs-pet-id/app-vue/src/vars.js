20c487704708fe5c705dd43aff2fa87ddea1e0bb
Ken Moini <ken@kenmoini.com>
2021-12-08 19:10:58 -0500

    bump

--- services/go-nodejs-pet-id/app-vue/src/vars.js
+++ services/go-nodejs-pet-id/app-vue/src/vars.js
@@ -1,7 +1,7 @@
 module.exports = {
-  KeycloakServer: "https://RH_SSO_HOST/auth/",
+  KeycloakServer: "https://RH_SSO_ROUTE/auth/",
 
-  PetIDServerEndpoint: "https://PET_ID_BACKEND_HOST/app",
+  PetIDServerEndpoint: "https://PET_ID_BACKEND_ROUTE/app",
 
   KeycloakRealm: "petcorp",
 
